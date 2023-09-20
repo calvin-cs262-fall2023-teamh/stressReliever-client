99226493250cb4befc42d1376d1c590b14140255
unknown <nihao6633839@gmail.com>
2023-09-20 16:33:26 -0400

    Added FidgetToy component and other enhancements

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
+// App.js
+
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import FidgetToy from './src/components/FidgetToy';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <FidgetToy />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +17,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
 });
+
---
+++ src/components/FidgetToy.js
@@ -0,0 +1,65 @@
+// src/components/FidgetToy.js
+
+import React, { useState, useEffect } from 'react';
+import { Animated, TouchableOpacity, Text, StyleSheet } from 'react-native';
+
+const FidgetToy = () => {
+  const [animatedValue] = useState(new Animated.Value(0));
+  const [colorIndex, setColorIndex] = useState(0);
+  
+  const colors = [
+    'rgb(123,104,238)',
+    'rgb(255,105,180)',
+    'rgb(50,205,50)',
+    'rgb(255,165,0)',
+    'rgb(64,224,208)'
+  ];
+
+  useEffect(() => {
+    Animated.timing(animatedValue, {
+      toValue: 1,
+      duration: 2000,
+      useNativeDriver: false,
+    }).start();
+  }, [colorIndex]);
+
+  const handlePress = () => {
+    setColorIndex((prevIndex) => (prevIndex + 1) % colors.length);
+  };
+
+  const animatedStyle = {
+    backgroundColor: animatedValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: [colors[colorIndex], colors[(colorIndex + 1) % colors.length]],
+    }),
+  };
+
+  return (
+    <Animated.View style={[styles.container, animatedStyle]}>
+      <TouchableOpacity style={styles.button} onPress={handlePress}>
+        <Text style={styles.buttonText}>Press Me</Text>
+      </TouchableOpacity>
+    </Animated.View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    backgroundColor: '#FFF',
+    paddingVertical: 16,
+    paddingHorizontal: 40,
+    borderRadius: 12,
+    elevation: 10,
+  },
+  buttonText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
+
+export default FidgetToy;
