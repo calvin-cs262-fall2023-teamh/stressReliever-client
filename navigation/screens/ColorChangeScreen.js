355ca20d799c3bdf5d97c4ce75e6ec3abb358909
Yuese Li <nihao6633839@gmail.com>
2023-11-18 14:47:40 -0500

    ColorChangeScreen.js eslint fix again

--- navigation/screens/ColorChangeScreen.js
+++ navigation/screens/ColorChangeScreen.js
@@ -1,12 +1,9 @@
 import * as React from 'react';
-import PropTypes from 'prop-types'; // Import PropTypes
 import { StatusBar } from 'expo-status-bar';
 import { View, StyleSheet } from 'react-native';
 import ColorChange from '../../fidgetComponents/ColorChange';
 
-export default function ColorChangeScreen({ navigation }) {
-    // Use navigation as needed
-
+export default function ColorChangeScreen() {
     return (
         <View style={styles.container}>
           <ColorChange />
@@ -14,10 +11,6 @@ export default function ColorChangeScreen({ navigation }) {
         </View>
     );
 }
-
-ColorChangeScreen.propTypes = {
-  navigation: PropTypes.object, // Validate navigation prop
-};
 
 const styles = StyleSheet.create({
     container: {
