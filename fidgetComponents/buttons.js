12b7fb5a610473d01e1ba8b1db524ca853794259
Yuese Li <nihao6633839@gmail.com>
2023-12-08 01:34:30 -0500

    updated fidget layout from tyler

--- fidgetComponents/buttons.js
+++ fidgetComponents/buttons.js
@@ -1,7 +1,5 @@
-// RoundButtonToolbox.js
-
 import React from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import RoundButton from './buttons/buttonOutline';
 
 const RoundButtonToolbox = () => {
@@ -10,17 +8,34 @@ const RoundButtonToolbox = () => {
   };
 
   return (
-    <View style={{ flexDirection: 'row', padding: 15 }}>
-      <View style={{ flexDirection: 'column' }}>
-        <RoundButton title='1' onPress={() => handleButtonPress(1)} backgroundColor={'red'} />
-        <RoundButton title='3' onPress={() => handleButtonPress(3)} backgroundColor={'blue'}/>
-      </View>
-      <View style={{ flexDirection: 'column' }}>
-        <RoundButton title='2' onPress={() => handleButtonPress(2)} backgroundColor={'yellow'} />
-        <RoundButton title='4' onPress={() => handleButtonPress(4)} backgroundColor={'green'} />
+    <View style={styles.toolContainer}>
+      <View style={{ flexDirection: 'row', padding: 15 }}>
+        <View style={{ flexDirection: 'column' }}>
+          <RoundButton title="1" onPress={() => handleButtonPress(1)} backgroundColor={"#6aa3f7"} />
+          <RoundButton title="3" onPress={() => handleButtonPress(3)} backgroundColor={"red"} />
+        </View>
+        <View style={{ flexDirection: 'column' }}>
+          <RoundButton title="2" onPress={() => handleButtonPress(2)} backgroundColor={"yellow"} />
+          <RoundButton title="4" onPress={() => handleButtonPress(4)} backgroundColor={"green"} />
+        </View>
       </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  toolContainer: {
+    backgroundColor: '#263238',
+    padding: 40,
+    borderRadius: 10,
+    marginBottom: 10,
+    width: 160,
+    height: 170,
+    marginTop: 10,
+    marginRight: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default RoundButtonToolbox;
