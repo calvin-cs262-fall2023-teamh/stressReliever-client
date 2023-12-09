8969ed5ad93452bae7bfcd5ef71776da601d089a
Yuese Li <nihao6633839@gmail.com>
2023-12-09 03:13:44 -0500

    Homepage Stats, i hope it works

---
+++ navigation/screens/ColorChangeTapsScreen.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
+
+const ColorChangeTapsScreen = () => {
+  const stats = [
+    { color: 'rgb(123,104,238)', taps: 80, time: '30s' },
+    { color: 'rgb(255,105,180)', taps: 70, time: '48s' },
+    { color: 'rgb(50,205,50)', taps: 75, time: '26s' },
+    { color: 'rgb(255,165,0)', taps: 65, time: '36s' },
+    { color: 'rgb(64,224,208)', taps: 45, time: '25s' },
+    { color: 'rgb(255,218,185)', taps: 67, time: '36s' },
+    { color: 'rgb(255,99,71)', taps: 77, time: '34s' },
+    { color: 'rgb(100,149,237)', taps: 56, time: '46s' },
+    { color: 'rgb(255,20,147)', taps: 66, time: '34s' },
+    { color: 'rgb(50,205,50)', taps: 58, time: '46s' }
+  ];
+
+  // Function to dynamically create text style based on color
+  const getTextStyle = (color) => ({
+    fontSize: 16,
+    color: color,
+    marginBottom: 5,
+  });
+
+  return (
+    <ScrollView style={styles.container}>
+      <Text style={styles.title}>Color Change Taps Details</Text>
+      {stats.map((stat, index) => (
+        <View key={index} style={[styles.statBox, {borderColor: stat.color}]}>
+          <Text style={getTextStyle(stat.color)}>
+            {`Color: ${stat.color}`}
+          </Text>
+          <Text style={styles.statText}>
+            {`Taps: ${stat.taps}`}
+          </Text>
+          <Text style={styles.statText}>
+            {`Time: ${stat.time}`}
+          </Text>
+        </View>
+      ))}
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+    textAlign: 'center',
+    marginVertical: 20,
+  },
+  statBox: {
+    borderWidth: 2,
+    borderRadius: 10,
+    padding: 15,
+    marginHorizontal: 20,
+    marginBottom: 15,
+  },
+  statText: {
+    fontSize: 16,
+    color: 'white',
+    marginBottom: 5,
+  },
+});
+
+export default ColorChangeTapsScreen;
--- navigation/screens/HomeScreen.js
+++ navigation/screens/HomeScreen.js
@@ -2,9 +2,11 @@ import React, { useContext } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import DarkModeContext from '../../color/DarkModeContext';
+import { useNavigation } from '@react-navigation/native';
 
 const HomeScreen = () => {
   const { darkMode } = useContext(DarkModeContext);
+  const navigation = useNavigation(); 
 
   const styles = StyleSheet.create({
     container: {
@@ -16,7 +18,7 @@ const HomeScreen = () => {
     header: {
       fontSize: 28,
       fontWeight: 'bold',
-      color: darkMode ? 'white' : 'white', // White for dark mode, dark gray for light mode
+      color: darkMode ? 'white' : 'white',
       marginBottom: 40,
       textAlign: 'center',
     },
@@ -67,7 +69,7 @@ const HomeScreen = () => {
       <Text style={styles.header}>Welcome to MindfulKnight!</Text>
 
       {/* Card: Previous Drawings */}
-      <TouchableOpacity style={styles.card} onPress={() => alert('Show previous drawings')}>
+      <TouchableOpacity style={styles.card} onPress={() => navigation.navigate('PreviousDrawingsScreen')}>
         <Ionicons name="brush-outline" size={40} color="#4A90E2" />
         <View style={styles.cardTextContainer}>
           <Text style={styles.cardTitle}>Previous Drawings</Text>
@@ -75,21 +77,21 @@ const HomeScreen = () => {
         </View>
       </TouchableOpacity>
 
-      {/* Card: Last Spin Counts */}
-      <TouchableOpacity style={styles.card} onPress={() => alert('Show last spin counts')}>
-        <Ionicons name="reload-outline" size={40} color="#F5A623" />
+      {/* Card: Last Fidget Session Review */}
+      <TouchableOpacity style={styles.card} onPress={() => navigation.navigate('LastFidgetSessionScreen')}>
+        <Ionicons name="time-outline" size={40} color="#F5A623" />
         <View style={styles.cardTextContainer}>
-          <Text style={styles.cardTitle}>Last Spin Counts</Text>
-          <Text style={styles.cardDetail}>You spun 5 times in the last session</Text>
+          <Text style={styles.cardTitle}>Last Fidget Session Review</Text>
+          <Text style={styles.cardDetail}>Review your last session activities</Text>
         </View>
       </TouchableOpacity>
 
       {/* Card: Color Change Taps */}
-      <TouchableOpacity style={styles.card} onPress={() => alert('Show color change taps')}>
+      <TouchableOpacity style={styles.card} onPress={() => navigation.navigate('ColorChangeTapsScreen')}>
         <Ionicons name="color-palette-outline" size={40} color="#A569BD" />
         <View style={styles.cardTextContainer}>
           <Text style={styles.cardTitle}>Color Change Taps</Text>
-          <Text style={styles.cardDetail}>You tapped 12 times in the last session</Text>
+          <Text style={styles.cardDetail}>Review the colors you tapped</Text>
         </View>
       </TouchableOpacity>
 
